524607c7e1ce75a0fc21489c134d944909d2cd92
GR8 <eminhwan12@gmail.com>
2021-02-23 14:35:08 +0900

    Create Project & register Domain

---
+++ models/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const User = require('./user');
+const Post = require('./post');
+const Hashtag = require('./hashtag');
+const Domain = require('./domain');
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+
+
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+db.Domain = Domain;
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+Domain.init(sequelize);
+
+User.associate(db);
+Post.associate(db);
+Hashtag.associate(db);
+Domain.associate(db);
+
+module.exports = db;
